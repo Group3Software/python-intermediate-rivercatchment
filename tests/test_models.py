9ab75bdd44af3dca4b71f2a98c58683366829f84
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2023-03-28 15:35:22 +0100

    Add two unit tests using the pytest.mark.parametrize

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import pandas.testing as pdt
 import datetime
-
+import pytest
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -65,10 +65,41 @@ def test_daily_max():
                      columns=['A', 'B']
     )
     test_result = pd.DataFrame(
-                     data=[[0.0, 0.1]],
+                     data=[[0.0, 0.0]],
                      index=[datetime.date(2000, 1, 1)],
                      columns=['A', 'B']
     )
 
     # Need to use Pandas testing functions to compare arrays
     pdt.assert_frame_equal(daily_max(test_input), test_result)
+
+@pytest.mark.parametrize(
+    "test_data, test_index, test_columns, expected_data, expected_index, expected_columns",
+    [
+        (
+            [[0.0, 0.0], [0.0, 0.0], [0.0, 0.0]],
+            [pd.to_datetime('2000-01-01 01:00'),
+             pd.to_datetime('2000-01-01 02:00'),
+             pd.to_datetime('2000-01-01 03:00')],
+            ['A', 'B'],
+            [[0.0, 0.0]],
+            [datetime.date(2000,1,1)],
+            ['A', 'B']),
+        ([[1, 2], [3, 4], [5, 6]],
+            [pd.to_datetime('2000-01-01 01:00'),
+             pd.to_datetime('2000-01-01 02:00'),
+             pd.to_datetime('2000-01-01 03:00')],
+            ['A', 'B'],
+            [[3.0, 4.0]],
+            [datetime.date(2000,1,1)],
+            ['A', 'B']
+         )
+    ]
+)
+def test_daily_mean(test_data, test_index, test_columns,
+                    expected_data, expected_index, expected_columns):
+    """Test mean function works"""
+    from catchment.models import daily_mean
+    pdt.assert_frame_equal(
+        daily_mean(pd.DataFrame(data=test_data, index=test_index, columns=test_columns)),
+        pd.DataFrame(data=expected_data, index=expected_index, columns=expected_columns))
