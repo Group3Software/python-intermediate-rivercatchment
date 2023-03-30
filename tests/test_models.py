d7a05b12bc54bcbcc96deb7b7d60544cdbc659db
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2023-03-30 12:50:48 +0100

    Plot shapefiles and create tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,6 +5,19 @@ import pandas.testing as pdt
 import datetime
 import pytest
 import numpy.testing as npt
+import geopandas as gpd
+from shapely.geometry import Point
+
+
+def test_create_site_with_position():
+    """Check a site is created correctly given a name."""
+    from catchment.models import Site
+    name = 'PL23'
+    longitude = 5  # This is a made up location for the unit test only; no actual data is accessed
+    latitude = 7
+    position = gpd.GeoDataFrame(geometry=[Point((longitude, latitude))], crs='EPSG:4326')  # Test that this line
+    p = Site(name=name, longitude=longitude, latitude=latitude)  # corresponds to this line
+    assert p.location.geom_equals(position)[0]
 
 
 @pytest.mark.parametrize(
@@ -37,20 +50,21 @@ def test_daily_min_python_list():
     from catchment.models import daily_min
 
     with pytest.raises(AttributeError):
-        error_expected = daily_min([[3, 4, 7],[-3, 0, 5]])
+        error_expected = daily_min([[3, 4, 7], [-3, 0, 5]])
+
 
 @pytest.mark.parametrize(
     "test_data, test_index, test_columns, expected_data, expected_index, expected_columns",
     [
         (
-            [ [0.0, 0.0], [0.0, 0.0], [0.0, 0.0] ],
-            [ pd.to_datetime('2000-01-01 01:00'),
-              pd.to_datetime('2000-01-01 02:00'),
-              pd.to_datetime('2000-01-01 03:00') ],
-            [ 'A', 'B' ],
-            [ [0.0, 0.0] ],
-            [ datetime.date(2000,1,1) ],
-            [ 'A', 'B' ]
+            [[0.0, 0.0], [0.0, 0.0], [0.0, 0.0]],
+            [pd.to_datetime('2000-01-01 01:00'),
+                pd.to_datetime('2000-01-01 02:00'),
+                pd.to_datetime('2000-01-01 03:00')],
+            ['A', 'B'],
+            [[0.0, 0.0]],
+            [datetime.date(2000, 1, 1)],
+            ['A', 'B']
         ),
         (
             [[1, 2], [3, 4], [5, 6]],
@@ -65,7 +79,7 @@ def test_daily_min_python_list():
     ]
 )
 def test_daily_mean(test_data, test_index, test_columns,
-                         expected_data, expected_index, expected_columns):
+                    expected_data, expected_index, expected_columns):
     """Test mean function works with zeros and positive integers"""
     from catchment.models import daily_mean
     pdt.assert_frame_equal(
@@ -126,6 +140,7 @@ def test_create_site():
     p = Site(name=name)
     assert p.name == name
 
+
 def test_create_catchment():
     """Check a catchment is created correctly given a name."""
     from catchment.models import Catchment
@@ -133,17 +148,20 @@ def test_create_catchment():
     catchment = Catchment(name=name)
     assert catchment.name == name
 
+
 def test_catchment_is_location():
     """Check if a catchment is a location."""
     from catchment.models import Catchment, Location
     catchment = Catchment("Spain")
     assert isinstance(catchment, Location)
 
+
 def test_site_is_location():
     """Check if a site is a location."""
     from catchment.models import Site, Location
     PL23 = Site("PL23")
     assert isinstance(PL23, Location)
+
 
 def test_sites_added_correctly():
     """Check sites are being added correctly by a catchment. """
@@ -153,6 +171,7 @@ def test_sites_added_correctly():
     catchment.add_site(PL23)
     assert catchment.sites is not None
     assert len(catchment.sites) == 1
+
 
 def test_no_duplicate_sites():
     """Check adding the same site to the same catchment twice does not result in duplicates. """
